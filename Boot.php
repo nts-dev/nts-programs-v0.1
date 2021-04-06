73d49f3d9c4d20592cec4befa839b0d9eeb8dd02
nts-dev <daniel@nts.nl>
2021-04-06 12:22:58 +0300

    changes

---
+++ Boot.php
@@ -0,0 +1,11 @@
+<?php
+
+
+class Boot {
+
+    const DBHOST = 'localhost';
+    const DBUSER =  'root';
+    const DBPASS = '';
+    const DBNAME = 'nts_projects';
+    const WWWROOT = 'https://localhost/nts-programs/';
+}
---
+++ includes.php
@@ -0,0 +1,3 @@
+<?php
+
+include 'Boot.php';
